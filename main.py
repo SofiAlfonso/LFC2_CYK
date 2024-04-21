67718991eac62493600700e654b2c29fab4ecc65
SofiAlfonso <asalfonsom@eafit.edu.co>
2024-04-21 16:37:06 -0500

    FINAL

--- main.py
+++ main.py
@@ -1,78 +1,90 @@
-
+"""
+TITTLE: Homework 2 of Formal languages and compilers
+PROGRAM: Implementation of CYK ALGORITHM  explained on Dexter C. Kozen
+AUTHORS: David Alejandro Ramirez and Ana Sofia Alfonso Moncada
+"""
+# Function to implement the CYK algorithm
 def CYK(G, str):
-    tamaño = len(str)
-    M =[]
-    for i in range(tamaño):
+    # Getting the length of the input string
+    size = len(str)
+    # Matrix to store intermediate results
+    M = []
+    for i in range(size):
         M.append([])
 
-#Completing the matrix
-    #First case, substring length = 1
-    i=0
+    # Completing the matrix
+    # First case, substring length = 1
+    i = 0
     for a in str:
-        nt=[]
+        nt = []
         for b in G:
             if a in G[b]:
-                for n in range (tamaño):
+                # Initialize cells with "-"
+                for n in range(size):
                     M[i].append("-")
+                # Add non-terminal symbol to the cell
                 nt.append(b)
-                M[i][i]=nt
-        i+=1
+                M[i][i] = nt
+        i += 1
 
-    #For length greater than 1
-    for l in range(2, tamaño+1):
-        for i in range (tamaño-l+1):
-            j= i+l-1
-            for k in range (i,j):
-                agregar=[]
+    # For length greater than 1
+    for l in range(2, size + 1):
+        for i in range(size - l + 1):
+            j = i + l - 1
+            for k in range(i, j):
+                agregar = []
+                # Checking all grammar rules
                 for b in G:
                     for a in G[b]:
-                        if  len(a)==2:
-                            nt=[]
+                        if len(a) == 2:
+                            nt = []
                             for c in a:
                                 nt.append(c)
-                            if (nt[0] in M[k][i]) and (nt[1] in M[j][k+1]):
+                            # If the substring can be formed using the grammar rules, add to list
+                            if (nt[0] in M[k][i]) and (nt[1] in M[j][k + 1]):
                                 agregar.append(b)
-                                M[j][i]=agregar
+                                M[j][i] = agregar
 
-#Verify if the string is accepted or not
-
-    if "S" in M[tamaño-1][0]:
+    # Verify if the string is accepted or not
+    if "S" in M[size - 1][0]:
         return "yes"
     else:
         return "No"
 
 
-
-
-
-
-
-def Inicio():
-    respuestas={}
+# Function to handle input and output
+def Start():
+    responses = {}
 
     G = {}
 
+    # Input grammar rules
     l = input().split(" ")
-    n_nt = l[0]
-    n_str = l[1]
+    n_nt = l[0]  # Number of non-terminal symbols
+    n_str = l[1]  # Number of strings to evaluate
     for h in range(int(n_nt)):
         r = input().split(" ")
-        G[r[0]]=[]
-        for m in range(len(r)-1):
-            G[r[0]].append(r[m+1])
+        G[r[0]] = []
+        for m in range(len(r) - 1):
+            G[r[0]].append(r[m + 1])
+    # Input strings
     for s in range(int(n_str)):
-        str= input()
-        respuesta= CYK(G,str)
-        respuestas[str]= respuesta
-    return respuestas
+        string = input()
+        response = CYK(G, string)
+        responses[string] = response
+    return responses
+
 
 if __name__ == '__main__':
-    casos = int(input())
-    coleccion= []
-    for i in range(casos):
-        respuesta= Inicio();
-        coleccion.append(respuesta)
+    # Input number of cases
+    cases = int(input())
+    collection = []
+    # Loop through each case
+    for i in range(cases):
+        response = Start()
+        collection.append(response)
 
-    for a in coleccion:
+    # Output results
+    for a in collection:
         for x in a:
             print(f"{x} -> {a[x]}")
